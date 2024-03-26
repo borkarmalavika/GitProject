afc5144aba233af92f7d42e8147b14f724cb68e2
borkar <borkar@LAPTOP-6IKVAIHT>
2024-03-26 14:08:51 +0530

    commit first commit

---
+++ src/sampleCode.java
@@ -0,0 +1,16 @@
+public class sampleCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="hungry";
+		if(str=="hungry")
+		{
+			System.out.print("matching");
+		}
+		else
+			System.out.print("not matching");
+
+	}
+
+}
+
