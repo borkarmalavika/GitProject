8644bd6165014089aa54fc4a685140cae1f36b34
borkar <borkar@LAPTOP-6IKVAIHT>
2024-03-26 14:25:41 +0530

    changed with mahathi

--- src/sampleCode.java
+++ src/sampleCode.java
@@ -2,13 +2,13 @@ public class sampleCode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str="hungry";
-		if(str=="hungry")
+		String str="MAHATHI";
+		if(str=="Mahathi")
 		{
 			System.out.print("matching");
 		}
 		else
-			System.out.print("not matching-Changed NOWWWWW");
+			System.out.print("not matching with MAHATHI");
 
 	}
 
