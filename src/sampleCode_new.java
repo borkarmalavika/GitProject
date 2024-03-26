136ec2f36f5142a13eaa740dcebe1ab069414c2f
borkar <borkar@LAPTOP-6IKVAIHT>
2024-03-26 15:44:01 +0530

    commting test2 to branch x

---
+++ src/sampleCode_new.java
@@ -0,0 +1,16 @@
+public class sampleCode_new {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="newwwwwwwwwwwwwwwwwww";
+		if(str=="Mahathi")
+		{
+			System.out.print("matching");
+		}
+		else
+			System.out.print("not matching with MAHATHI");
+
+	}
+
+}
+
